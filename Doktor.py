dcd954f24203c14db0f7cb6d5b85368249f9cca5
Berzan8 <128976199+Berzan8@users.noreply.github.com>
2024-06-07 18:45:41 +0300

    Update Doktor.py

--- Doktor.py
+++ Doktor.py
@@ -1,4 +1,4 @@
-from personel import *
+from personel import personel
 class Doktor(personel):
     def __init__(self, personel_no, ad, soyad, departman, maas,uzmanlik, deneyim_yili, hastane):
         super().__init__(personel_no, ad, soyad, departman, maas)
@@ -10,45 +10,32 @@ class Doktor(personel):
         return self.uzmanlik
     
     def set_uzmanlik(self,uzmanlik):
-        try:
-            if isinstance(uzmanlik,str) and uzmanlik.strip():
-                self.uzmanlik = uzmanlik
-            else:
-                raise ValueError("girilen uzmanlik bulunmadi!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.uzmanlik=uzmanlik
+        return uzmanlik
             
     def get_deneyim_yili(self):
         return self.deneyim_yili
     
     def set_deneyim_yili(self,deneyim_yili):
-        try:
-            if isinstance(deneyim_yili,int) and deneyim_yili.strip():
-                self.uzmanlik = deneyim_yili
-            else:
-                raise ValueError("girilen deneyim yili geçersiz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.deneyim_yili=deneyim_yili
+        return self.deneyim_yili
             
     def get_hastane(self):
         return self.hastane
     
     def set_hastane(self,hastane):
-        try:
-            if isinstance(hastane,int) and hastane.strip():
-                self.hastane = hastane
-            else:
-                raise ValueError("girilen deneyim yili geçersiz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.hastane=hastane
+        return hastane
     
-    def __str__(self, personel_no, ad, soyad, departman, maas,uzmanlik,deneyim_yili,hastane):
-        return print(f"{super().__str__(personel_no, ad, soyad, departman, maas)} uzmanlik:{uzmanlik}\ndeneyim yili:{deneyim_yili}\n hastane:{hastane}")
+    def __str__(self):
+        return print(f"{super().__str__()} uzmanlik:{self.uzmanlik}\ndeneyim yili:{self.deneyim_yili}\n hastane:{self.hastane}")
     
-    def maas_artir(self,deneyim_yili):
-        if deneyim_yili<0:
-            print("maaş artirmak için en az bir yil çalismaniz gerekir.")
-            
-        else :
-            yenimaas=self.maas*(deneyim_yili/10)+self.maas
-            print("yeni maas:",yenimaas)
+    def maas_artir(self,oran):
+        yeni_maas=self.set_maas(self.get_maas() * (1 + oran))
+        return print("yeni maasiniz:",yeni_maas)
+    
+yeni=Doktor(12,"berzan","erdal","kbb",19000,"kulak",3,"cigli")
+yeni.__str__()
