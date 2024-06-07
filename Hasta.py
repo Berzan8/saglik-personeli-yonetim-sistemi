cc58b6a9e1f7418b7ec129699df05a5576ee47d2
Berzan8 <128976199+Berzan8@users.noreply.github.com>
2024-06-07 19:08:04 +0300

    Update Hasta.py

--- Hasta.py
+++ Hasta.py
@@ -1,5 +1,3 @@
-from Doktor import *
-from Hemsire import *
 import datetime
 
 class Hasta:
@@ -14,47 +12,35 @@ class Hasta:
     def get_hasta_no(self):
         return self.hasta_no
     def set_hasta_no(self,hasta_no):
-        try:
-            if isinstance(hasta_no,int) and hasta_no.strip():
-                self.hasta_no = hasta_no
-            else:
-                raise ValueError("Lütfen rakamlardan olusan çalişma  saatinizi giriniz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")     
+        
+        self.hasta_no=hasta_no
+        return self.hasta_no     
     
     def get_ad(self):
         return self.ad
     
     def set_ad(self,ad):
-        try:
-            if isinstance(ad,int) and ad.strip():
-                self.ad = ad
-            else:
-                raise ValueError("Lütfen adinizi giriniz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.ad=ad
+        return self.ad
             
     def get_soyad(self):
         return self.soyad
     
     def set_soyad(self,soyad):
-        try:
-            if isinstance(soyad,int) and soyad.strip():
-                self.soyad = soyad
-            else:
-                raise ValueError("Lütfen soyadinizi giriniz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.soyad=soyad
+        return self.soyad
     
     def get_dogum_tarihi(self):
         return self.dogum_tarihi
     
     def set_dogum_tarihi(self,dogum_tarihi):
-        dogum_tarihi = input("Tarihi 'YYYY-MM-DD' formatında girin: ")
-
         try:
+            
             girilen_tarih = datetime.strptime(dogum_tarihi, "%Y-%m-%d")
             print(f"Girilen tarih: {girilen_tarih}")
+            
         except ValueError:
             print("Geçersiz tarih formatı. Lütfen 'YYYY-MM-DD' formatında bir tarih girin.")
     
@@ -62,29 +48,21 @@ class Hasta:
         return self.hastalik
     
     def set_hastalik(self,hastalik):
-        try:
-            if isinstance(hastalik,int) and hastalik.strip():
-                self.hastalik = hastalik
-            else:
-                raise ValueError("Lütfen hastaliginizi giriniz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+        
+        self.hastalik=hastalik
+        return self.hastalik
             
     def get_tedavi(self):
         return self.soyad
     
     def set_tedavi(self,tedavi):
-        try:
-            if isinstance(tedavi,int) and tedavi.strip():
-                self.tedavi = tedavi
-            else:
-                raise ValueError("Lütfen tedavinizi giriniz!!...")
-        except ValueError as e:
-            print(f"Hata: {e}")
+       
+       self.tedavi=tedavi
+       return self.tedavi
             
-    def __str__(self,hasta_no,ad,soyad,dogum_tarihi,hastalik,tedavi):
+    def __str__(self):
         
-        return print(f"hasta_no:{hasta_no}\n ad:{ad}soyad:{soyad}\n dogum_tarihi:{dogum_tarihi}\n hastalik:{hastalik}\tedavi:{tedavi}")
+        return print(f"hasta_no:{self.hasta_no}\nad:{self.ad}soyad:{self.soyad}\ndogum_tarihi:{self.dogum_tarihi}\nhastalik:{self.hastalik}\ntedavi:{self.tedavi}")
     
     def tedavi_suresi_hesapla(baslangic,bitis):
         baslangic=input("tedavi baslangic tarihinizi 'YYYY-MM-DD' şeklinde giriniz ")
@@ -103,3 +81,6 @@ class Hasta:
         
         tedavi_süresi=bitis-baslangic
         print(f"Bugün ile girilen tarih arasındaki gün farkı: {tedavi_süresi.days} gün")
+        
+yeni=Hasta(12,"gjkg","jhgjkhg","22-12-1222","gsd0","fgdfhdf")
+yeni.__str__()
